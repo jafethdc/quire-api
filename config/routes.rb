2d326f8ef0c17c0bd5a9b672678ba832123eb06e
jafethdc <jafeth.diazc@gmail.com>
2016-12-19 01:35:33 -0500

    removes subdomain constraint due to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:create] do
         resources :products do
