5f4159d528ff382e51fc7422e6b895f37734ff2a
jafethdc <jafeth.diazc@gmail.com>
2016-12-19 18:08:22 -0500

    modifies nested user serializer for product

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :description, :price, :created_at
   belongs_to :seller
+
+  class UserSerializer < ActiveModel::Serializer
+    attributes :id, :username, :email, :full_name
+  end
 end
