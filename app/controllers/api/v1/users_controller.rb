d72b77382a4238769f74dc1364484edf9b4500d3
jafethdc <jafeth.diazc@gmail.com>
2017-04-29 20:52:42 -0500

    removes show and create actions in products

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,21 +4,6 @@ module Api
       before_action :authenticate_with_token, only: [:update]
       before_action only: [:update] { match_token_with_user(:id) }
 
-      def show
-        user = User.find(params[:id])
-        render json: user, status: 200
-      end
-
-      def create
-        fb_profile = validate_fb_user(params[:fb_access_token])
-        user = User.new(user_params.merge(access_token: generate_api_token).merge(fb_profile.slice(:email, :name)))
-        if user.save
-          render json: user, status: 201
-        else
-          render json: { errors: user.errors.full_messages }, status: 422
-        end
-      end
-
       def update
         if logged_user.update(user_params)
           render json: logged_user, status: 200
